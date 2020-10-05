210fa3e8e75a5c3323c38ee0957cd3fdf9dc6d73
gabrielsluz <gabrielsluz1999@gmail.com>
2020-10-05 16:47:57 -0300

    Commit 2: refatoracao com Money e mais alteracoes

--- Main.java
+++ Main.java
@@ -1,12 +1,13 @@
 import static org.junit.Assert.*;
 import org.junit.Test;
 import src.Dollar;
+import src.Franc;
 
 
 
 public class Main{
 @Test
-public void testMultiplication() {
+public static void testMultiplication() {
    Dollar five = new Dollar(5);
    assertEquals(new Dollar(10), five.times(2));
    assertEquals(new Dollar(15), five.times(3));
@@ -16,11 +17,24 @@ public void testMultiplication() {
 public static void testEquality() {
    assertTrue(new Dollar(5).equals(new Dollar(5)));
    assertFalse(new Dollar(5).equals(new Dollar(6)));
+   assertTrue(new Franc(5).equals(new Franc(5)));
+   assertFalse(new Franc(5).equals(new Franc(6)));
 }
+
+@Test
+public static void testFrancMultiplication() {
+   Franc five = new Franc(5);
+   assertEquals(new Franc(10), five.times(2));
+   assertEquals(new Franc(15), five.times(3));
+}
+
 
   public static void main(String arg[]) {
     System.out.println("hello world");
+    testMultiplication();
     testEquality();
+
+    testFrancMultiplication();
   }
 
 }
